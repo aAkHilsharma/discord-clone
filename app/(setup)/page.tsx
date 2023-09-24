142ae9904ce139cfc4d18653b88f4184c7e77633
aAkHilsharma <akhilsharma6721@gmail.com>
2023-09-24 14:04:39 +0530

    Organizational changes to main page

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initail-profile";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  // search all the members in the servers and find the first one that has the profile id in one of the members of that server
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div> Create a server </div>;
+};
+
+export default SetupPage;
---
+++ lib/initail-profile.ts
@@ -0,0 +1,34 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+// get current user from clerk and check if there is already a profile associated with the user then return the profile other create a new profile and return new profile
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
