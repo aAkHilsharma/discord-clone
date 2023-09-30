7c5686b6ff180c0aa0bec447eff80bb0f89a5637
aAkHilsharma <akhilsharma6721@gmail.com>
2023-09-30 09:08:57 +0530

    minor changes

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -29,7 +29,7 @@ import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
   name: z.string().min(1, {
-    message: "Server name is required.",
+    message: "Server name is needed.",
   }),
   imageUrl: z.string().min(1, {
     message: "Server image is required.",
